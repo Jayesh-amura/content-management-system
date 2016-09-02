79d7ea0d37ba8f481f90bbb04282586c78775741
jayesh <jayesh@amuratech.com>
2016-09-02 17:33:30 +0530

    create user migration

---
+++ db/migrate/20160902115230_create_users.rb
@@ -0,0 +1,31 @@
+class CreateUsers < ActiveRecord::Migration
+  #--------TYPE-----------
+  #binary boolean date datetime decimal
+  #float integer string text time
+
+  #--------OPTIONS--------
+  # :limit => size
+  # :default => value
+  # :null => true/false
+  # :precision => number
+  # :scale => number
+  # :default => ""
+  def up
+    create_table :users do |t|
+      #t.column "name", :type, options
+      #t.type "name", options
+      t.column "first_name", :string , :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => "", :null => false
+      t.string "password", :limit => 40
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
+      t.timestamps null: false
+    end
+  end
+
+  def down
+  	drop_table :users
+  end
+
+end
