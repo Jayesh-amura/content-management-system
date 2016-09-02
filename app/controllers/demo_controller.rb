ec6aeb70428d001759ae99999cb3819a8734654b
jayesh <jayesh@amuratech.com>
2016-09-02 17:11:46 +0530

    params in hello and index view and demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,9 +3,21 @@ class DemoController < ApplicationController
   def index
   	#render(:template => "demo/hello")
   	#render("demo/hello")
-  	render("hello")#render => just setting the template to use
+  	#render("hello")#render => just setting the template to use
   end
   def hello
-  	render("index")
+  	#render("index")
+    @array = [1,2,3,4,5]
+    @id = params['id'].to_i
+    @page = params[:page].to_i
   end
+
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end
+
+  def lynda
+  	redirect_to("https://mail.google.com/mail/#inbox")
+  end
+
 end
